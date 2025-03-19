8351371617f3277fe43b12c8e76276170249002e
Display-Ketersediaan-Laboran <donnipernanda90@gmail.com>
2025-03-19 23:53:06 +0700

    di resources/view/users/add footer and navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,14 @@
+@vite('resources/css/app.css')
+<nav class=" sticky top-0 z-10">
+<div class=" bg-[#2F81DF] flex justify-between p-0 m-0">
+    <div class="flex items-center text-[2.5rem] text-white">
+    <img src="{{asset('img/footer.png')}}" alt="nav" class="w-24 h-17 m-5 ml-20 p-0">SKIL
+    </div>
+    <div class="flex items-center text-2xl mr-5 gap-20 text-white ">
+        <div>Beranda</div>
+        <div>Contact US</div>
+        <div>About US</div>
+        <div class="ml-15">Login</div>
+    </div>
+</div>
+</nav>
